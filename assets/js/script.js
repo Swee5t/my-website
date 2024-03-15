ad8ef86dad5b366f172a8bffa4d844dc43dd4b0b
PhuongVy_ <tphuc6861@gmail.com>
2024-03-16 02:47:39 +0700

    update

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Get all close buttons
+    const closeButtons = document.querySelectorAll('.close-button');
+
+    // Add click event listener to each close button
+    closeButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            // Hide the popup by setting its display property to 'none'
+            document.querySelector('.popup').style.display = 'none';
+        });
+    });
+});
