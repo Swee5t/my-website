e129434304b8fecd763c39f8f72433abb57b3968
PhuongVy_ <tphuc6861@gmail.com>
2024-03-16 22:20:33 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
     developerBadge.addEventListener('click', function() {
         document.querySelector('.popup-container').style.display = 'flex';
         document.querySelector('.popup .logo').style.display = 'flex';
-        document.querySelector('.popup .text').textContent = 'Certified Developer Badge';
+        document.querySelector('.popup .text').textContent = 'Huy hiệu nhà phát triển được chứng nhận';
         document.querySelector('.popup .description').textContent = 'Được trao cho những cá nhân có kiến thức sâu rộng và kinh nghiệm đáng kể, huy hiệu này là một dấu hiệu của sự tôn trọng và công nhận từ cộng đồng đối với những đóng góp và thành tựu của họ.';
         document.querySelectorAll('.close-button').forEach(element => {
             element.style.display = 'flex';
