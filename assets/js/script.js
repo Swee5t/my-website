c6ca344a9c2a9f929fd4b6f25debd49e029e0a8a
PhuongVy_ <tphuc6861@gmail.com>
2024-03-16 18:25:02 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Get all close buttons
     const closeButtons = document.querySelectorAll('.close-button');
+    const developerBadge = document.querySelector('.title .badge');
 
-    // Add click event listener to each close button
     closeButtons.forEach(function(button) {
         button.addEventListener('click', function() {
-            // Hide the popup by setting its display property to 'none'
-            document.querySelector('.popup').style.display = 'none';
-            document.querySelector('body').style.overflow = 'visible';
+            document.querySelector('.popup-container').style.display = 'none';
+        });
+    });
+
+    developerBadge.addEventListener('click', function() {
+        document.querySelector('.popup-container').style.display = 'flex';
+        document.querySelector('.popup .logo').style.display = 'flex';
+        document.querySelector('.popup .text').textContent = 'Certified Developer Badge';
+        document.querySelector('.popup .description').textContent = 'Được trao cho những cá nhân có kiến thức sâu rộng và kinh nghiệm đáng kể, huy hiệu này là một dấu hiệu của sự tôn trọng và công nhận từ cộng đồng đối với những đóng góp và thành tựu của họ.';
+        document.querySelectorAll('.close-button').forEach(element => {
+            element.style.display = 'flex';
         });
     });
 });
