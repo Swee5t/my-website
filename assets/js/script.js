0dd32508d0a7a6a66cced0b5e7f94b2a04c6f984
PhuongVy_ <tphuc6861@gmail.com>
2024-03-17 17:32:34 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,45 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const closeButtons = document.querySelectorAll('.close-button');
-    const developerBadge = document.querySelector('.title .badge');
+document.addEventListener('DOMContentLoaded', () => {
+    const badge = document.querySelector('.title .badge');
+    const popup = document.querySelector('.popup-container');
+    const popupLogo = document.querySelector('.popup .contents .logo');
+    const popupText = document.querySelector('.popup .contents .text');
+    const popupDescription = document.querySelector('.popup .contents .description');
+    const buttons = document.querySelectorAll('.popup .contents .button');
 
-    closeButtons.forEach(function(button) {
-        button.addEventListener('click', function() {
-            document.querySelector('.popup-container').style.display = 'none';
-        });
+    if (popup) {
+        popup.style.display = 'flex';
+        popupLogo.style.display = 'none';
+        popupText.textContent = 'o/ Hi';
+        popupDescription.textContent = 'Vì nhà phát triển trang này quá lười, các tính năng có thể không hoạt động trên iOS hoặc Android.';
+    }
+
+    buttons.forEach((element) => {
+        buttons[0].textContent = 'Okey';
+        buttons[0].onclick = () => {
+            popup.style.display = 'none';
+        };
+        buttons[1].textContent = 'Xem source code (GitHub)';
+        buttons[1].onclick = () => {
+            popup.style.display = 'none';
+            window.open('https://github.com/Swee5t/my-website', '_blank');
+        };
     });
 
-    developerBadge.addEventListener('click', function() {
-        document.querySelector('.popup-container').style.display = 'flex';
-        document.querySelector('.popup .logo').style.display = 'flex';
-        document.querySelector('.popup .text').textContent = 'Certified Developer Badge';
-        document.querySelector('.popup .description').textContent = "This account has been certified since February 17, 2024.";
-        const buttons = document.querySelectorAll('.close-button');
+    badge.onclick = () => {
+        popup.style.display = 'flex';
+        popupLogo.style.display = 'flex';
+        popupText.textContent = 'Certified Developer Badge';
+        popupDescription.textContent = 'This account has been certified since February 17, 2024.';
         buttons.forEach((element) => {
-            buttons[0].textContent = 'Okey';
+            buttons[0].textContent = 'OK';
+            buttons[0].onclick = () => {
+                popup.style.display = 'none';
+            };
             buttons[1].textContent = 'Claim your badge';
+            buttons[1].onclick = () => {
+                popup.style.display = 'none';
+                window.open('https://youtu.be/dQw4w9WgXcQ?si=7TIxLwsimzDqoHC7', '_blank'); // hehe
+            };
         });
-    });
+    }
 });
